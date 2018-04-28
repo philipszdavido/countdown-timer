7ca5e788edb96fed6321b9998faa1b7ea84369e6
philipsz-davido <philipsz-davido>
2018-04-28 19:33:30 +0100

    Added babel config

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,13 @@
 import uglify from 'rollup-plugin-uglify'
+import babel from 'rollup-plugin-babel'
 
 const config = {
     input: 'src/countdown.js',
     external: ['react'],
     plugins: [
+        babel({
+            exclude: "node_modules/**"
+        }),
         uglify()
     ],
     output: {
